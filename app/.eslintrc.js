0870289e979e1ae6cd7225800290907ec642ee7e
富山涼平 <tennisnowboarderyou@gmail.com>
2019-07-21 00:33:05 +0900

    とりあえずAR表示できた

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -5,12 +5,13 @@ module.exports = {
   },
   extends: [
     'plugin:vue/essential',
-    '@vue/airbnb',
+    'airbnb-base',
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'semi': ['error', 'never'],
+    'max-len': ['error', { 'code': 100, 'ignoreComments': true, 'ignoreTrailingComments': true, 'ignoreUrls': true, 'ignoreStrings': true, 'ignoreTemplateLiterals': true, 'ignoreRegExpLiterals': true }],
   },
   parserOptions: {
     parser: 'babel-eslint',
--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -1,14 +1,49 @@
-<template lang="pug">
-  div hoge
+<template>
+  <a-scene embedded arjs v-if="show">
+    <a-marker preset="hiro">
+        <a-box position='0 0.5 0' material='color: yellow;'></a-box>
+    </a-marker>
+    <a-entity camera></a-entity>
+  </a-scene>
 </template>
 
 <script>
-import 'aframe'
-
 export default {
   name: 'home',
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      show: false,
+    }
+  },
+  methods: {
+    loadScript({ src, id }) {
+      if (!document) {
+        return
+      }
+      const script = document.createElement('script')
+      script.src = src
+      if (id) {
+        script.id = id
+      }
+      document.body.appendChild(script)
+    },
+    async loadPromise(id) {
+      return new Promise((resolve, reject) => {
+        const $el = document.querySelector(id)
+        if (!$el) {
+          reject()
+        }
+        $el.addEventListener('load', () => {
+          resolve()
+        })
+      })
+    },
+  },
+  async mounted() {
+    this.loadScript({ src: 'https://aframe.io/releases/0.9.2/aframe.min.js', id: 'aframe-script' })
+    this.loadScript({ src: 'https://cdn.rawgit.com/jeromeetienne/AR.js/1.7.5/aframe/build/aframe-ar.js', id: 'aframe-ar-script' })
+    await Promise.all([this.loadPromise('#aframe-script'), this.loadPromise('#aframe-ar-script')])
+    this.show = true
   },
 }
 </script>
