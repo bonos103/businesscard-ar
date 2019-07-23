73c827f4a3a6ca49932e522c86d6b3bfcfc8c7f9
富山涼平 <tennisnowboarderyou@gmail.com>
2019-07-23 23:04:43 +0900

    設定変更

--- app/vue.config.js
+++ app/vue.config.js
@@ -1,7 +1,22 @@
 module.exports = {
   devServer: {
     host: '0.0.0.0',
+    https: true,
     disableHostCheck: true,
+    compress: true,
+    stats: 'normal',
+    clientLogLevel: 'debug',
+    quiet: false,
+  },
+  configureWebpack: (config) => { // eslint-disable-line
+    return {
+      optimization: {
+        splitChunks: {
+          name: 'vendor',
+          chunks: 'initial',
+          minChunks: 2,
+        },
+      },
+    }
   },
 }
-
