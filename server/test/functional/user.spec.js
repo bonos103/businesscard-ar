58f1ce5c8d7066fdea1ab8d472c622b267b5cf65
富山涼平 <tennisnowboarderyou@gmail.com>
2019-07-30 22:41:11 +0900

    test POST /api/user

---
+++ server/test/functional/user.spec.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const { test, trait } = use('Test/Suite')('User')
+const Factory = use('Factory')
+
+const UserFactory = Factory.model('App/Models/User')
+
+trait('Test/ApiClient')
+
+test('can create a challenge if valid data', async ({ assert, client }) => {
+  const { email, password } = await UserFactory.make()
+  const data = {
+    email,
+    password,
+  }
+  const response = await client.post('/api/user').send(data).end()
+  response.assertStatus(201)
+  response.assertJSONSubset({
+    email,
+  })
+})
