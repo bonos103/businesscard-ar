d422c1e157594dc8f73fec88bc8b73981b883306
富山涼平 <tennisnowboarderyou@gmail.com>
2019-07-30 21:32:29 +0900

    try Factory

--- server/test/unit/user.spec.js
+++ server/test/unit/user.spec.js
@@ -1,14 +1,16 @@
 'use strict'
 
 const { test, trait } = use('Test/Suite')('User')
+const Factory = use('Factory')
 const User = use('App/Models/User')
 
 trait('DatabaseTransactions')
 
 test('dirty password', async ({ assert }) => {
   const user = new User()
-  user.email = 'hoge@hoge.com'
-  user.password = 'password'
+  const { email, password } = await Factory.model('App/Models/User').make()
+  user.email = email
+  user.password = password
   const result = await user.save()
-  assert.notEqual(user.password, 'password', `result: ${result}`)
+  assert.notEqual(user.password, password, `result: ${result}`)
 })
