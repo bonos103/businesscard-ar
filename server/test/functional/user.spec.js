348bfa3e6883391d54a976e61abfbfde9a07e34e
富山涼平 <tennisnowboarderyou@gmail.com>
2019-08-01 23:17:19 +0900

    ユーザー登録のテスト

--- server/test/functional/user.spec.js
+++ server/test/functional/user.spec.js
@@ -1,6 +1,9 @@
 'use strict'
 
+const moment = require('moment')
+
 const { test, trait } = use('Test/Suite')('User')
+const Encryption = use('Encryption')
 const Factory = use('Factory')
 
 const UserFactory = Factory.model('App/Models/User')
@@ -85,4 +88,53 @@ test('cannot create a user if no password', async ({ assert, client }) => {
       validation: 'required',
     },
   ])
+})
+
+test('メール認証：認証コードが不正', async ({ client }) => {
+  const illegalCode = 'hogehgoe'
+  const data = {
+    code: 'hogehgoe',
+  }
+  const response = await client.post('/api/v1/user/auth').send(data).end()
+  response.assertStatus(400)
+  response.assertJSONSubset([
+    {
+      message: '認証に失敗しました',
+      field: 'code',
+      validation: 'invalid',
+    },
+  ])
+})
+test('メール認証：認証コードが期限切れ', async ({ client }) => {
+  const { email, password } = await UserFactory.make()
+  const createdDate = moment().subtract(1, 'days')
+  const code = Encryption.encrypt(JSON.stringify({ email, password, createdDate }))
+  const data = {
+    code,
+  }
+  const response = await client.post('/api/v1/user/auth').send(data).end()
+  response.assertStatus(400)
+  response.assertJSONSubset([
+    {
+      message: '期限切れです。',
+      field: 'code',
+      validation: 'codeTimeout',
+    },
+  ])
+})
+test('メール認証：認証コードが正しい場合は、ユーザー作成', async ({ assert, client }) => {
+  const { email, password } = await UserFactory.make()
+  const createdDate = moment()
+  const code = Encryption.encrypt(JSON.stringify({ email, password, createdDate }))
+  const data = {
+    code,
+  }
+  const response = await client.post('/api/v1/user/auth').send(data).end()
+
+  response.assertStatus(200)
+  response.assertJSONSubset({
+    message: '登録完了しました。',
+    user: { email, password },
+  })
+  assert.exists(response.token)
 })
