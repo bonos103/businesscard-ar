6f41503721ed8b12b9251d769de7deeaa225e893
富山涼平 <tennisnowboarderyou@gmail.com>
2019-08-04 09:57:04 +0900

    serverにeslint導入

---
+++ server/.eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  rules: {
+    'strict': ["error", "global"],
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'semi': ['error', 'never'],
+    'max-len': ['error', { 'code': 100, 'ignoreComments': true, 'ignoreTrailingComments': true, 'ignoreUrls': true, 'ignoreStrings': true, 'ignoreTemplateLiterals': true, 'ignoreRegExpLiterals': true }],
+  },
+  parserOptions: {
+    sourceType: 'script',
+  },
+  globals: {
+    use: true,
+  },
+}
