b30d70661bff84a3edfaa8eb9d10ec60258d1ba8
富山涼平 <tennisnowboarderyou@gmail.com>
2019-08-11 22:23:13 +0900

    install postcss-preset-env

--- app/postcss.config.js
+++ app/postcss.config.js
@@ -1,5 +1,30 @@
+const path = require('path')
+
+function resolve(p) {
+  return path.join(__dirname, p)
+}
+
 module.exports = {
   plugins: {
-    autoprefixer: {},
+    // autoprefixer: {},
+    'postcss-preset-env': {
+      stage: 3,
+      features: {
+        'nesting-rules': true,
+        'color-mod-function': true,
+        'media-query-ranges': true,
+        'custom-media-queries': {
+          importFrom: [
+            resolve('./src/assets/stylesheets/media.css'),
+          ],
+        },
+        // 'custom-properties': {
+        //   preserve: false,
+        //   importFrom: [
+        //     resolve('./src/assets/stylesheets/variables.css'),
+        //   ],
+        // },
+      },
+    },
   },
 }
