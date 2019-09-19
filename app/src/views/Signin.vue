6e4e89f52539ea57c004d27a0d632216e73cc512
富山涼平 <tennisnowboarderyou@gmail.com>
2019-09-19 22:20:10 +0900

    signin見た目だけ

--- app/src/views/Signin.vue
+++ app/src/views/Signin.vue
@@ -1,18 +1,36 @@
 <template lang="pug">
   div(:class="$style.wrapper")
-    div
+    div(:class="$style.inner")
       h1(:class="$style.title")
         span 名刺作成ツール
         | QRAR
-      form
+      validation-observer(
+        tag="form",
+        ref="observer",
+        @submit.prevent="handleSubmit",
+        :class="$style.form",
+        novalidate,
+      )
         form-control
-          label(:class="$style.label") Email
-          a-input(placeholder="input your email address.", type="email")
+          label(:class="$style.label") E-mail
+          text-field(
+            name="E-mail",
+            rules="required|email",
+            v-model="email",
+            placeholder="input your email address.",
+            type="email",
+          )
         form-control
           label(:class="$style.label") Password
-          a-input(placeholder="半角英数 6文字以上", type="password")
+          text-field(
+            name="Password",
+            rules="required|alpha_dash|min:6",
+            v-model="password",
+            placeholder="半角英数 6文字以上",
+            type="password",
+          )
         div(:class="$style.buttonBlock")
-          a-button(size="large", ghost) ログイン
+          a-button(htmlType="submit", size="large", ghost) ログイン
 </template>
 <style module>
   .wrapper {
@@ -25,6 +43,9 @@
     padding-left: 20px;
     padding-right: 20px;
   }
+  .inner {
+    width: 100%;
+  }
   .title {
     color: #fff;
     font-size: 2rem;
@@ -34,6 +55,11 @@
     @media (--md) {
       font-size: 2.4rem;
     }
+  }
+  .form {
+    max-width: 400px;
+    margin-left: auto;
+    margin-right: auto;
   }
   .label {
     color: #fff;
@@ -45,11 +71,36 @@
   }
 </style>
 <script>
+import { ValidationObserver } from 'vee-validate'
 import FormControl from '@/components/Form/Control.vue'
+import TextField from '@/components/Form/TextField.vue'
 
 export default {
   components: {
+    ValidationObserver,
     FormControl,
+    TextField,
+  },
+  data() {
+    return {
+      email: '',
+      password: '',
+    }
+  },
+  methods: {
+    async handleSubmit() {
+      const isValid = await this.$refs.observer.validate()
+      Object.values(this.$refs.observer.refs).forEach(provider => provider.setFlags({
+        dirty: true,
+        touched: true,
+      }))
+      if (isValid) {
+        await this.submit()
+      }
+    },
+    submit() {
+      console.log('submit!')
+    },
   },
 }
 </script>
