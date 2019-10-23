fc0325b9274247749c62726ed6750d2ba82b0b94
富山涼平 <tennisnowboarderyou@gmail.com>
2019-10-23 23:03:02 +0900

    プロジェクト取得テスト

---
+++ server/test/functional/get-project.spec.js
@@ -0,0 +1,34 @@
+'use strict'
+
+const { test, trait } = use('Test/Suite')('CreateProject')
+const Factory = use('Factory')
+
+const ItemFactory = Factory.model('App/Models/Item')
+const ProjectFactory = Factory.model('App/Models/Project')
+const UserFactory = Factory.model('App/Models/User')
+
+trait('DatabaseTransactions')
+trait('Test/ApiClient')
+trait('Auth/Client')
+
+test('can get project only owner', async ({ client }) => {
+  const user = await UserFactory.create()
+  const project = await ProjectFactory.create()
+  const item = await ItemFactory.create()
+
+  await project.user().associate(user)
+  await item.project().associate(project)
+
+  const response = await client
+  .get(`/api/v1/project/${project.id}`)
+  .loginVia(user, 'jwt')
+  .end()
+
+  response.assertStatus(200)
+  response.assertJSONSubset({
+    ...project1.toJSON(),
+    items: [
+      item.toJSON(),
+    ],
+  })
+})
