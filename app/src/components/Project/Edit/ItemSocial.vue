000916835e106be4f345328c9f538a51f9401c7f
RyoheiTomiyama <tennisnowboarderyou@gmail.com>
2019-11-29 22:52:39 +0900

    ソーシャル画像をitemに依存させない

--- app/src/components/Project/Edit/ItemSocial.vue
+++ app/src/components/Project/Edit/ItemSocial.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
   item-block(:item="item", :active="active")
     template(v-slot="{ textStyle, handleChangeValue }")
-      img(:src="item.image", :class="$style.image")
+      img(:src="iconImage", :class="$style.image")
 </template>
 <style module>
   .image {
@@ -11,6 +11,9 @@
 </style>
 <script>
 import ItemBlock from '@/components/Project/Edit/ItemBlock.vue'
+import twitterIcon from '@/assets/images/project/sns/twitter.png'
+import facebookIcon from '@/assets/images/project/sns/facebook.png'
+import instagramIcon from '@/assets/images/project/sns/instagram.png'
 
 export default {
   props: {
@@ -19,6 +22,21 @@ export default {
   },
   components: {
     ItemBlock,
+  },
+  computed: {
+    iconImage() {
+      const match = this.item.value.match(/twitter|facebook|instagram/)
+      if (match[0] === 'twitter') {
+        return twitterIcon
+      }
+      if (match[0] === 'facebook') {
+        return facebookIcon
+      }
+      if (match[0] === 'instagram') {
+        return instagramIcon
+      }
+      return ''
+    },
   },
 }
 </script>
