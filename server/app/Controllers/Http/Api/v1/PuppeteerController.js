41d9e78f74d645fa8530d0ec8aae91d5ec0aa1e4
RyoheiTomiyama <tennisnowboarderyou@gmail.com>
2019-12-07 00:44:22 +0900

    あとちょっとで行けそう・・・ bearerトークンをheaderに入れたい！

--- server/app/Controllers/Http/Api/v1/PuppeteerController.js
+++ server/app/Controllers/Http/Api/v1/PuppeteerController.js
@@ -2,21 +2,37 @@
 
 const puppeteer = require('puppeteer')
 
+const Logger = use('Logger')
+
 class PuppeteerController {
-  async index({ response }) {
+  async index({ request, response }) {
+    const authorization = request.header('Authorization')
+    Logger.info(authorization)
     const browser = await puppeteer.launch({
       args: ['--no-sandbox', '--disable-setuid-sandbox'],
       executablePath: '/usr/bin/chromium-browser',
       ignoreHTTPSErrors: true,
     })
     const page = await browser.newPage()
-    await page.goto('https://app:8080/', { waitUntil: 'networkidle2' })
+    await page.setRequestInterception(true)
+    page.on('request', (req) => {
+      // Override headers
+      // const headers = {
+      //   ...req.headers(),
+      //   Authorization: authorization,
+      // }
+      Logger.info(JSON.stringify(req.headers()))
+      req.continue()
+      // req.continue({ headers })
+    })
+    await page.setExtraHTTPHeaders({ "Authorization": authorization })
+    await page.goto('https://app:8080/project/', { waitUntil: 'networkidle2' })
     const image = await page.screenshot({
       // encoding: 'base64',
     })
 
     await browser.close()
-    response.json(image)
+    response.type('image/png').json(image)
   }
 }
 
