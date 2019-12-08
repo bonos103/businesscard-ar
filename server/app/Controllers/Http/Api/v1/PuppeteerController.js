857c0d76a44216919fc95541ba20868628bb40af
RyoheiTomiyama <tennisnowboarderyou@gmail.com>
2019-12-08 22:17:03 +0900

    tokenを渡すのは多分できたが、
    appからauth check APIを叩くときのURLがhttps://0.0.0.0:3333/...になっているので、docker上のchrominiumからアクセスできないのがエラーの原因
    devServerの設定のproxy設定してhttps://server:3333でAPIを叩けるようにする！

--- server/app/Controllers/Http/Api/v1/PuppeteerController.js
+++ server/app/Controllers/Http/Api/v1/PuppeteerController.js
@@ -5,28 +5,44 @@ const puppeteer = require('puppeteer')
 const Logger = use('Logger')
 
 class PuppeteerController {
-  async index({ request, response }) {
-    const authorization = request.header('Authorization')
-    Logger.info(authorization)
+  async index({ auth, response }) {
+    const user = await auth.getUser()
+    const jwtToken = await auth.generate(user)
+    Logger.info(JSON.stringify(jwtToken))
+    // const authorization = request.header('Authorization')
+    // Logger.info(authorization)
     const browser = await puppeteer.launch({
       args: ['--no-sandbox', '--disable-setuid-sandbox'],
       executablePath: '/usr/bin/chromium-browser',
       ignoreHTTPSErrors: true,
     })
     const page = await browser.newPage()
-    await page.setRequestInterception(true)
-    page.on('request', (req) => {
-      // Override headers
-      // const headers = {
-      //   ...req.headers(),
-      //   Authorization: authorization,
-      // }
-      Logger.info(JSON.stringify(req.headers()))
-      req.continue()
-      // req.continue({ headers })
+
+    // await page.setRequestInterception(true)
+    // page.on('request', (req) => {
+    //   // Override headers
+    //   // const headers = {
+    //   //   ...req.headers(),
+    //   //   Authorization: authorization,
+    //   // }
+    //   Logger.info(JSON.stringify(req.headers()))
+    //   req.continue()
+    //   // req.continue({ headers })
+    // })
+
+    // await page.setExtraHTTPHeaders({ authorization: `Bearer ${jwtToken.token}` })
+
+    await page.setCookie({
+      name: 'token',
+      value: jwtToken.token,
+      url: 'https://app:8080/',
+      expires: new Date('3000/12/12').getTime(),
+      secure: true,
     })
-    await page.setExtraHTTPHeaders({ "Authorization": authorization })
+    // const res = await page.goto('https://app:8080/api/v1/user/check', { waitUntil: 'networkidle2' })
+    // Logger.info(await res.text())
     await page.goto('https://app:8080/project/', { waitUntil: 'networkidle2' })
+    // Logger.info(JSON.stringify(await page.cookies('https://app:8080/project/')))
     const image = await page.screenshot({
       // encoding: 'base64',
     })
