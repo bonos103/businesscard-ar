14fc914f837f918a08a87cdb5a6fa4b1a710f8cd
RyoheiTomiyama <tennisnowboarderyou@gmail.com>
2019-12-10 23:35:12 +0900

    600x600でのスクショに成功！

--- server/app/Controllers/Http/Api/v1/PuppeteerController.js
+++ server/app/Controllers/Http/Api/v1/PuppeteerController.js
@@ -2,35 +2,22 @@
 
 const puppeteer = require('puppeteer')
 
-const Logger = use('Logger')
-
 class PuppeteerController {
   async index({ auth, response }) {
     const user = await auth.getUser()
     const jwtToken = await auth.generate(user)
-    Logger.info(JSON.stringify(jwtToken))
-    // const authorization = request.header('Authorization')
-    // Logger.info(authorization)
+
     const browser = await puppeteer.launch({
       args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      defaultViewport: {
+        width: 1200,
+        height: 1200,
+        deviceScaleFactor: 0.75,
+      },
       executablePath: '/usr/bin/chromium-browser',
       ignoreHTTPSErrors: true,
     })
     const page = await browser.newPage()
-
-    // await page.setRequestInterception(true)
-    // page.on('request', (req) => {
-    //   // Override headers
-    //   // const headers = {
-    //   //   ...req.headers(),
-    //   //   Authorization: authorization,
-    //   // }
-    //   Logger.info(JSON.stringify(req.headers()))
-    //   req.continue()
-    //   // req.continue({ headers })
-    // })
-
-    // await page.setExtraHTTPHeaders({ authorization: `Bearer ${jwtToken.token}` })
 
     await page.setCookie({
       name: 'token',
@@ -41,10 +28,16 @@ class PuppeteerController {
     })
     // const res = await page.goto('https://app:8080/api/v1/user/check', { waitUntil: 'networkidle2' })
     // Logger.info(await res.text())
-    await page.goto('https://app:8080/project/', { waitUntil: 'networkidle2' })
+    await page.goto('https://app:8080/project/1/edit', { waitUntil: 'networkidle2' })
     // Logger.info(JSON.stringify(await page.cookies('https://app:8080/project/')))
     const image = await page.screenshot({
       // encoding: 'base64',
+      clip: {
+        x: 200,
+        y: 200,
+        width: 800,
+        height: 800,
+      },
     })
 
     await browser.close()
