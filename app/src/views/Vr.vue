631063e7172a01cc2d056c7e7f32157e7345d5ea
RyoheiTomiyama <tennisnowboarderyou@gmail.com>
2020-09-09 22:05:46 +0900

    ARにマテリアルの表示・ホバーポイント問題ないが、マーカーに合わせて回転しない、、、

--- app/src/views/Vr.vue
+++ app/src/views/Vr.vue
@@ -15,6 +15,7 @@
 <script>
 // eslint-disable
 import * as THREE from 'three'
+import LoadScript from '@/utils/LoadScript'
 
 export default {
   data() {
@@ -29,12 +30,19 @@ export default {
       raycaster: new THREE.Raycaster(),
       mouseVector: new THREE.Vector3(),
       selectedObject: null,
+      arToolkitSource: null,
+      arToolkitContext: null,
     }
   },
   methods: {
     animate() {
-      this.renderer.render(this.scene, this.camera)
       requestAnimationFrame(this.animate)
+      if (this.arToolkitSource.ready === false) {
+        return
+      }
+      this.renderer.render(this.scene, this.camera)
+      this.arToolkitContext.update(this.arToolkitSource.domElement)
+      this.scene.visible = this.camera.visible
     },
     onWindowResize() {
       const w = window.innerWidth
@@ -42,6 +50,13 @@ export default {
       this.camera.aspect = w / h
       this.camera.updateProjectionMatrix()
       this.renderer.setSize(w, h)
+
+      // AR
+      this.arToolkitSource.onResizeElement()
+      this.arToolkitSource.copyElementSizeTo(this.renderer.domElement)
+      if (this.arToolkitContext.arController !== null) {
+        this.arToolkitSource.copyElementSizeTo(this.arToolkitContext.arController.canvas)
+      }
     },
     getIntersects(x, y) {
       this.mouseVector.set(
@@ -70,34 +85,39 @@ export default {
     },
     createMaterials() {
       // add sprites
-      const sprite1 = new THREE.Sprite(new THREE.SpriteMaterial({ color: '#69f' }))
-      sprite1.position.set(6, 5, 5)
+      const sprite1 = new THREE.Sprite(new THREE.SpriteMaterial({ color: '#0f0' }))
+      sprite1.position.set(1, 0, 1)
       sprite1.scale.set(2, 5, 1)
       this.group.add(sprite1)
 
-      const sprite2 = new THREE.Sprite(new THREE.SpriteMaterial({ color: '#69f', sizeAttenuation: false }))
+      const sprite2 = new THREE.Sprite(new THREE.SpriteMaterial({ color: '#00f', sizeAttenuation: false }))
       sprite2.material.rotation = Math.PI / 3 * 4
-      sprite2.position.set(8, -2, 2)
+      sprite2.position.set(2, 0, 1)
       sprite2.center.set(0.5, 0)
       sprite2.scale.set(0.1, 0.5, 0.1)
       this.group.add(sprite2)
 
       const group2 = new THREE.Object3D()
       group2.scale.set(1, 2, 1)
-      group2.position.set(-5, 0, 0)
+      group2.position.set(-2, 0, 0)
       group2.rotation.set(Math.PI / 2, 0, 0)
       this.group.add(group2)
 
-      const sprite3 = new THREE.Sprite(new THREE.SpriteMaterial({ color: '#69f' }))
-      sprite3.position.set(0, 2, 5)
+      const sprite3 = new THREE.Sprite(new THREE.SpriteMaterial({ color: '#0ff' }))
+      sprite3.position.set(0, 0, 3)
       sprite3.scale.set(10, 2, 3)
       sprite3.center.set(-0.1, 0)
       sprite3.material.rotation = Math.PI / 3
       group2.add(sprite3)
     },
   },
-  mounted() {
+  async mounted() {
     window.THREE = THREE
+    const loadThreeAr = new LoadScript('https://raw.githack.com/AR-js-org/AR.js/master/three.js/build/ar.js', 'three-ar-script', 'pbody').load()
+    await Promise.all([loadThreeAr])
+
+    const { THREEx } = window
+    THREEx.ArToolkitContext.baseURL = 'https://localhost:8080/'
 
     // init this.renderer
     this.renderer.setClearColor(new THREE.Color('lightgrey'), 0)
@@ -106,15 +126,40 @@ export default {
     document.body.appendChild(this.renderer.domElement)
 
     // init this.scene
-    this.scene.background = new THREE.Color(0xffffff)
+    // this.scene.background = new THREE.Color(0xffffff)
     this.scene.add(this.group)
 
     // init this.camera
-    this.camera.position.set(15, 15, 15)
-    this.camera.lookAt(this.scene.position)
+    // this.camera.position.set(15, 15, 15)
+    // this.camera.lookAt(this.scene.position)
+    this.scene.add(this.camera)
 
     // init material
     this.createMaterials()
+
+    this.arToolkitSource = new THREEx.ArToolkitSource({
+      sourceType: 'webcam',
+    })
+    this.arToolkitContext = new THREEx.ArToolkitContext({
+      cameraParametersUrl: `${THREEx.ArToolkitContext.baseURL}data/camera_para.dat`,
+      detectionMode: 'mono',
+    })
+    this.markerControls = new THREEx.ArMarkerControls(this.arToolkitContext, this.camera, {
+      type: 'pattern',
+      patternUrl: `${THREEx.ArToolkitContext.baseURL}data/patt.hiro`,
+      // patternUrl : THREEx.ArToolkitContext.baseURL + '../data/data/patt.kanji',
+      // as we controls the camera, set changeMatrixMode: 'cameraTransformMatrix'
+      changeMatrixMode: 'cameraTransformMatrix',
+    })
+    this.scene.visible = false
+
+    this.arToolkitSource.init(() => {
+      setTimeout(this.onWindowResize, 2000)
+    })
+    this.arToolkitContext.init(() => {
+      // copy projection matrix to camera
+      this.camera.projectionMatrix.copy(this.arToolkitContext.getProjectionMatrix())
+    })
 
     window.addEventListener('resize', this.onWindowResize, false)
     window.addEventListener('mousemove', this.onDocumentMouseMove, false)
