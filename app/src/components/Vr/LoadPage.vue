040a682889cbe5c4fce38a85f5501c0b004fc804
RyoheiTomiyama <tennisnowboarderyou@gmail.com>
2020-09-13 22:22:16 +0900

    add loading

---
+++ app/src/components/Vr/LoadPage.vue
@@ -0,0 +1,40 @@
+<template lang="pug">
+  div(:class="$style.wrapper")
+    transition(name="load")
+      div(:class="$style.block", v-if="visible")
+</template>
+<script>
+export default {
+  props: {
+    visible: {
+      type: Boolean,
+      default: true,
+    },
+  },
+}
+</script>
+<style lang="postcss" module>
+  .block {
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    left: 0;
+    top: 0;
+    background-color: red;
+    z-index: 2;
+    overflow: hidden;
+    transition: all 5s;
+  }
+  .wrapper {
+    & :global {
+      & .load-enter-active,
+      & .load-leave-active {
+        transition: opacity 0.8s;
+      }
+      & .load-enter,
+      & .load-leave-to /* .fade-leave-active below version 2.1.8 */ {
+        opacity: 0;
+      }
+    }
+  }
+</style>
