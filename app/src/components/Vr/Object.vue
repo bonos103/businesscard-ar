318d8e7b3de5a19d784e9284c54531d798aa69b6
RyoheiTomiyama <tennisnowboarderyou@gmail.com>
2020-09-18 21:43:04 +0900

    fix ar align

--- app/src/components/Vr/Object.vue
+++ app/src/components/Vr/Object.vue
@@ -29,7 +29,7 @@ export default {
       return {
         fontSize: `${(this.item.font_size * 2) || 36}px`,
         color: this.item.color || 'blue',
-        textAlign: 'left',
+        textAlign: this.item.align || 'left',
         width: `${this.item.width * 200 || 900}px`,
         height: `${this.item.height * 200 || 300}px`,
       }
